8f45a49dfadea7e7abc61df026d2059b241264c0
M4theus1 <matheus.silva.alves2909@gmail.com>
2025-05-20 22:17:20 -0300

    Ajustes 22:17
    Ajustes de Commit

--- demo/demo/src/main/java/com/example/demo/service/ReceitaService.java
+++ demo/demo/src/main/java/com/example/demo/service/ReceitaService.java
@@ -1,51 +1,18 @@
 package com.example.demo.service;
 
+import com.example.demo.model.entity.ReceitaEntity;
 
-import com.example.demo.dto.ReceitaResponse;
-import com.example.demo.entity.ReceitaEntity;
-import com.example.demo.exception.NotFoundException;
-import com.example.demo.repository.ReceitaRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
-import java.awt.print.Pageable;
+public interface ReceitaService {
 
-@Service
-public class ReceitaService {
+    ReceitaEntity createReceita(ReceitaEntity receita);
 
-    private final ReceitaRepository repositoryReceita;
+    ReceitaEntity getReceitaById(Integer id);
 
-    public ReceitaService(ReceitaRepository repository) {
-        this.repositoryReceita = repository;
-    }
+    List<ReceitaEntity> getAllReceitas();
 
-    public void createReceita(ReceitaEntity receita){
-        repositoryReceita.save(receita);
-    }
+    ReceitaEntity updateReceita(Integer id, ReceitaEntity receitaDetails);
 
-    public Page<ReceitaEntity> getReceitas(Pageable pageable){
-        return findAll();
-    }
-
-    public Page<ReceitaEntity> findAll(){
-        int page = 0;
-        int size = 10;
-        PageRequest pageRequest = PageRequest.of(
-                page,
-                size,
-                Sort.Direction.ASC,
-                "titulo");
-        return new PageImpl<>(
-                repositoryReceita.findAll(),
-                pageRequest, size);
-    }
-    public ReceitaResponse getReceitaById(Long id) {
-        return repositoryReceita.findById(id)
-                .map(u -> new ReceitaResponse(u.getTitulo()))
-                .orElseThrow(() -> new NotFoundException("Receita não encontrado."));
-    }
-
+    void deleteReceita(Integer id);
 }

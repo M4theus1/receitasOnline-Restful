9f964b4904aea1cda0a8600a44e989ffccebf1af
WagnerFO <wagnerf.oliveira@unifacol.edu.br>
2025-05-22 17:44:17 -0300

    quase

--- demo/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ demo/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API de Receitas Gourmet")
+                        .title("API de Receitas")
                         .description("Esta API permite gerenciar receitas e avaliações dos usuários.")
                         .version("1.0.0")
                 );
